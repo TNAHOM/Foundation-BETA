b3cd6a1aa65ee77f46ea23593f9614d1aef92352
TNAHOM <nahomt2419@gmail.com>
2023-07-13 14:14:51 -0600

    dd

---
+++ dummy.py
@@ -0,0 +1,12 @@
+lists = [
+	['Nahom', '11', 'Addis Abeba', 'Jan 19 2022'],
+	['Yoseph', '11', 'Addis Abeba', 'Jan 19 2022'],
+	['Saron', '11', 'Addis Abeba', 'Jan 12 2022'],
+	['Kalab', '11', 'Addis Abeba', 'Jan 19 2022'],
+	['Abeselom', '12', 'Addis Abeba', 'Jan 12 2022'],
+	['Kirubel', '12', 'Addis Abeba', 'Jan 19 2022'],
+	['Yoseph', '12', 'Addis Abeba', 'Jan 19 2022']
+]
+
+# for x in lists:
+	#
