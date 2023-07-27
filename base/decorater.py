3253c9c3e48b4dcc68eac2aaaf0bf016ce2c9c1e
TNAHOM <nahomt2419@gmail.com>
2023-07-27 04:48:49 -0600

    Revert "Removed the qrcode and exam code added"
    This reverts commit 16ee67518ca633c877a9edc9118bfc1d1cbdba66.

--- base/decorater.py
+++ base/decorater.py
@@ -27,6 +27,14 @@ def allowed_user(allowed_roles=[]):
 				return view_func(request, *args, **kwargs)
 			
 			else:
+				if request.user.role=='Super User':
+					return redirect('superuser', request.user.id)
+				elif request.user.role=='Teacher':
+					return redirect('teacher', request.user.id)
+				elif request.user.role=='Student':
+					return redirect('student', request.user.id)
+				elif request.user.role=='School Administrator':
+					return redirect('administrator', request.user.id)
 				return redirect('login')
 	
 		return wrapper_func
--- base/utilities.py
+++ base/utilities.py
@@ -1,40 +1,64 @@
-import qrcode
-<<<<<<< HEAD
-
-import qrcode
-=======
 from random import randint
->>>>>>> parent of 24b8ebd (setup for production)
 
 def squash(dicts):
-	true_f = []
 	fill = []
-	choose = []
+	ans_tf = []
+	ans_other = []
+	ans_ch = []
 	for x, y in dicts.items():
 		if y !='':
-			if x[:4] == 'true':
-				true_f.append(y)
+			if x[:6] == 'ans_tf':
+				if y == 'true':
+					ans_tf.append('T')
+				elif y == 'false':
+					ans_tf.append('F')
+			# IF INPUT TEXT IS GIVEN
+			elif x[:5] == 'other':
+				ans_other.append(y.upper())
+			elif x[:6] == 'ans_ch':
+				ans_ch.append(y[0].upper())
 			elif x[:4] == 'fill':
-				fill.append(y)
-			elif x[:7]=='choose_':
-				choose.append(y)
-	return true_f, fill, choose
+				fill.append(y.upper())
+	# print(fill, 'fill')
+	# print(ans_tf, 'tf')
+	# print(ans_other, 'match')
+	# print(ans_ch, 'choose')
+	return fill, ans_tf, ans_other, ans_ch
 
-<<<<<<< HEAD
-def generate_qrcode(subject):
-	qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, border=0, box_size=4)
-	qr.add_data(subject)
-=======
-def generate_qrcode(subject, side):
-	qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, border=0, box_size=3)
-	info = subject, side
-	qr.add_data(info)
->>>>>>> parent of 24b8ebd (setup for production)
-	qr.make(fit=True)
-	
-	img = qr.make_image()
-	return img
+# def generate_qrcode(subject, side):
+# 	qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, border=0, box_size=3)
+# 	info = subject, side
+# 	qr.add_data(info)
+# 	qr.make(fit=True)
+#
+# 	img = qr.make_image()
+# 	return img
 
 def remove_stuff(original, removed):
 	chosen = original.translate({ord(letter): None for letter in f"{removed}"})
 	return chosen
+
+def exam_code(code_list):
+	while True:
+		exam_id_list=''
+		for x in range(5):
+			exam_id_list+=str(randint(1, 5))
+			
+		exam_code_f = ''
+		exam_code_f+=exam_id_list
+		exam_code_f+='1'
+		
+		exam_code_b = ''
+		exam_code_b += exam_id_list
+		exam_code_b += '2'
+		# print(len(code_list))
+		print(int(exam_code_f), int(exam_code_b), type(code_list))
+		if type(code_list) is list:
+			if int(str(exam_code_f)[:-1]) not in code_list:
+				return int(exam_code_f), int(exam_code_b)
+		elif type(code_list) is int:
+			print('------')
+			if int(str(exam_code_f)[:-1]) != code_list:
+				return int(exam_code_f), int(exam_code_b)
+		# elif code_list
+		
