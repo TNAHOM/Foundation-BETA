b7576eda26615789e7a1796177b69b03a9b082c7
Nahom Tamirat <77017384+TNAHOM@users.noreply.github.com>
2023-08-02 10:58:26 -0600

    Update settings.py
    debug false

--- SMS2/settings.py
+++ SMS2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+zv)qk(qjf#4c3#-omqb()t!ofo&lsai+p4^wd&!uiggek5yz2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['foundation-beta-production.up.railway.app', 'school-wise.com', 'localhost', '127.0.0.1']
